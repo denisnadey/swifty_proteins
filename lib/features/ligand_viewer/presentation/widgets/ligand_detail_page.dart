656f670d76d02dd9cd6a4e975fa1a16c5dd4e787
Denis Nadey <denis.nadey@gmail.com>
2024-12-29 14:20:00 +0200

    feat: add url_launcher and share_plus plugins for enhanced functionality

--- lib/features/ligand_viewer/presentation/widgets/ligand_detail_page.dart
+++ lib/features/ligand_viewer/presentation/widgets/ligand_detail_page.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:native_screenshot_widget/native_screenshot_widget.dart';
 import 'package:ngl_flutter/ngl_flutter.dart';
+
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
 class LigandDetailPage extends StatefulWidget {
   final String ligand;
@@ -13,6 +21,50 @@ class LigandDetailPage extends StatefulWidget {
 class _LigandDetailPageState extends State<LigandDetailPage> {
   String atomInfo = '';
 
+  final screenshotController = NativeScreenshotController();
+
+  Future<Uint8List?> takeScreenshot() async {
+    Uint8List? image;
+    final bytes = await screenshotController.takeScreenshot(scale: 1);
+    image = bytes;
+
+    return image;
+  }
+
+  Future<void> captureAndShare(BuildContext context) async {
+    try {
+      await takeScreenshot().then((Uint8List? image) async {
+        if (image != null) {
+          final directory = await getApplicationDocumentsDirectory();
+          final imagePath = await File('${directory.path}/image.png').create();
+          await imagePath.writeAsBytes(image);
+
+          await Share.shareXFiles(
+            [XFile(imagePath.path)],
+            text: 'Ligand: ${widget.ligand}',
+          );
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Изображение успешно создано')),
+            );
+          }
+        } else {
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Ошибка: не удалось создать изображение')),
+            );
+          }
+        }
+      });
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Ошибка: $e')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,35 +72,45 @@ class _LigandDetailPageState extends State<LigandDetailPage> {
         title: Text(
           "Selected ligand: ${widget.ligand}\n selectedAtom: $atomInfo",
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          ),
+          Gap(16),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () => captureAndShare(context),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Stack(
+      body: NativeScreenshot(
+        controller: screenshotController,
+        child: Column(
           children: [
-            Positioned.fill(
-              child: NglFlutter(
-                ligand: widget.ligand,
-                onAtomReceived: (model) {
-                  setState(() {
-                    atomInfo = '''element: ${model.element},
-                    index:  ${model.index},
-                    residue: ${model.residue},
-                    ''';
-                  });
-                },
-              ),
+            Text(
+              "Details and 3D viewer for Ligand: ${widget.ligand}",
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
             ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                color: Colors.white,
-                child: Text(
-                  "Details and 3D viewer for Ligand: ${widget.ligand}",
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
+            Gap(16),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: NglFlutter(
+                      ligand: widget.ligand,
+                      onAtomReceived: (model) {
+                        setState(() {
+                          atomInfo = '''element: ${model.element},
+                          index:  ${model.index},
+                          residue: ${model.residue},
+                          ''';
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import local_auth_darwin
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
